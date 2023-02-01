dbe2d1d8ac572be3f2e2bd03a8ecbe44f5b9c0f6
Gregory1331 <113695198+Gregory1331@users.noreply.github.com>
2023-02-01 15:04:29 +0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,60 @@
+// let elem = document.querySelector('#elem');
+// console.log(elem);
+// let button = document.querySelector("#button");
+//      button.addEventListener("click",func)
+//      function func(){
+//         elem.disabled = true;  
+//      }  
+//      let button2 = document.querySelector("#button2");
+//      button2.addEventListener("click",func2)
+//      function func2(){
+//         elem.disabled = false;  
+//      } 
+
+
+// let elem = document.querySelector('#elem');
+// console.log(elem.checked);
+// let button = document.querySelector("#button");
+//      button.addEventListener("click",func)
+//      function func(){
+//         elem.checked = true;  
+//      }  
+//      let button2 = document.querySelector("#button2");
+//      button2.addEventListener("click",func2)
+//      function func2(){
+//         elem.checked = false;  
+//      } 
+
+// let elem   = document.querySelector('#elem');
+// let button = document.querySelector('#button'); 
+// button.addEventListener('click', function() {
+// 	elem.checked = !elem.checked;
+// });
+
+// let radios = document.querySelectorAll('input[type="radio"]');
+// let button = document.querySelector('#button');
+// button.addEventListener('click',function(){
+//    for (let radio of radios){
+//       if(radio.checked){
+//          console.log(radio.value);
+//       }
+//    }
+// });
+
+// let elem = document.querySelector('#elem');
+// let p = document.querySelector('#p');
+// elem.addEventListener('change', function() {
+	
+//    p.textContent= this.value
+// });
+
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('change', function() {
+// 	console.log(this.value);
+// });
+
+ 
+   
+	
+
+
